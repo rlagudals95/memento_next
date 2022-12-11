3816f0d5df9bfe5404ceed01e281ba8512b98b00
gudals95 <102935090+gudals95@users.noreply.github.com>
2022-12-11 16:12:41 +0900

    feat: state에 접근 중..

--- src/components.tsx/CountTimer.tsx
+++ src/components.tsx/CountTimer.tsx
@@ -1,41 +1,52 @@
-import React, { useEffect, useState, useCallback, memo } from "react";
+import React, { useEffect, useState, useCallback, useRef, memo } from "react";
 import { SEX } from "@/constants";
+import { useRouter } from "next/router";
 
 const CountTimer = () => {
+  const router = useRouter();
+  const counterRef = useRef<HTMLInputElement>(null);
   const [lifeSeconds, setLifeSeconds] = useState(0);
-  const [test, setTest] = useState(1000);
 
-
-  
-  const setLifeExpectancyAndStartCounting = useCallback(() => {
+  const lifeExpectancy = useCallback(() => {
     const birthday = localStorage.getItem("birthday") as unknown as string;
+    if (!birthday) {
+      router.replace("/");
+    }
     const lifeExpectancyBySex =
       localStorage.getItem("sex") === SEX.FEMALE ? 80 : 83;
 
     const lifeExpectancyDate =
       `${String(parseInt(birthday.substring(0, 4)) + lifeExpectancyBySex)}` +
       String(birthday.substr(4, 8));
-    const lifeExpectancySeconds = new Date(lifeExpectancyDate).getTime() / 1000;
 
+    const lifeExpectancySeconds = new Date(lifeExpectancyDate).getTime() / 1000;
     setLifeSeconds(lifeExpectancySeconds);
+    return lifeExpectancySeconds;
   }, []);
+
+
+  const renewalLifeExpectancy = useCallback(() => {
+    localStorage.setItem("lifeSeconds", String(counterRef?.current?.value));
+  }, [lifeSeconds]);
 
   useEffect(() => {
-    setLifeExpectancyAndStartCounting();
+    lifeExpectancy();
+    const myInterval = setInterval(() => {
+      setLifeSeconds((lifeSeconds) => lifeSeconds - 1);
+    }, 1000);
 
-    function setTimeOutRecursion() {
-      setTimeout(function () {
-        console.log("count")
-        setTest(test - 1);
-        setTimeOutRecursion();
-      }, 1000);
-    }
-
-    setTimeOutRecursion();
-    // /return setTimeOutRecursion();
+    return () => {
+      clearInterval(myInterval);
+      
+      renewalLifeExpectancy();
+    };
   }, []);
 
-  return <>{test}</>;
+  return (
+    <div>
+      <input onChange={(e) => {console.log('ccc', e)}} readOnly={true} type="text" ref={counterRef} value={lifeSeconds} />
+    </div>
+  );
 };
 
-export default memo(CountTimer);
+export default CountTimer;
