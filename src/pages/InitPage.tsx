155b4d2760dd4a2f825f698ff30d6b9125d64482
rlagudals95 <dbfudgudals@naver.com>
2023-05-12 23:20:27 +0900

    feat: step

--- src/pages/InitPage.tsx
+++ src/pages/InitPage.tsx
@@ -7,7 +7,7 @@ const InitPage = () => {
 
   const [step, setStep] = useState(1);
 
-  const Container = styled.div`
+  const Container = styled.div` 
     font-size: ${FontSize.XLarge};
     font-weight: 900;
     color: black;
@@ -18,6 +18,7 @@ const InitPage = () => {
       {/* 당신의 남은 시간을 보여줄게요<br />
       우리 인생은 한정된 기간이에요<br />
       지금부터 당신의 시간을 함께해요<br /> */}
+      {step === 1}
       <div>
         죽음이 멀리 있고 대수롭지 않은 것으로 느껴질 때에는 우리 삶은 게을러져. <br />
         하지만 언제라도 죽음과 만나리라는 것을 알았을 때 인간은 보다 나은 삶을 살아갈 수 있지.<br />
