1fcfe015ee72323f46b7ce92dcff0053c7f07042
rlagudals95 <dbfudgudals@naver.com>
2023-05-26 00:33:20 +0900

    feat: oauth init

---
+++ src/pages/KakaoOAuthCallBackPage.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { LoginMethod } from "@apis/authApi";
+import ResponsiveContainer from "@components/web/ResponsiveContainer";
+import { useOauthStore } from "../../store/oauth";
+
+const KakaoOAuthCallBackPage = () => {
+  const { userInfo, OAuthRequest } = useOauthStore();
+
+  const params = new URL(document.URL).searchParams;
+  const code = params.get("code") as string;
+
+  const navigate = useNavigate();
+
+  const fetchUserInfoAndSet = async () => {
+    const loginResult = await OAuthRequest({
+      loginMethod: LoginMethod.KAKAO,
+      code,
+    });
+
+    if (loginResult.registeredUser) {
+      if (localStorage.getItem("accessToken")) {
+        if (window.ReactNativeWebView) {
+          postMessage({ accessToken: localStorage.getItem("accessToken") });
+        } else {
+          navigate("/");
+        }
+      }
+    } else {
+      navigate("/signUpProcessPage");
+    }
+  };
+
+  React.useEffect(() => {
+    if (!code) {
+      navigate("/");
+    }
+    fetchUserInfoAndSet();
+  }, []);
+
+  return (
+    <div>로그인 중 입니다..</div>
+  );
+};
+
+export default KakaoOAuthCallBackPage;
+
+
