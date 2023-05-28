b5a7afd5e9f1b81528cbc232ecb4f4661c793820
rlagudals95 <dbfudgudals@naver.com>
2023-05-28 17:06:46 +0900

    refactor: lint fix

--- src/components/CountTimer.tsx
+++ src/components/CountTimer.tsx
@@ -6,65 +6,6 @@ import { FontSize } from "@/constants/style";
 import SettingsBackupRestoreIcon from '@mui/icons-material/SettingsBackupRestore';
 import { removeUserInfo } from "@/utils/AppConfig";
 
-const CountTimer = () => {
-  const router = useRouter();
-  const [lifeSeconds, setLifeSeconds] = useState(0);
-  const [name, setName] = useState(localStorage.getItem('name') || null)
-  const [birthday, setBirthDay] = useState(localStorage.getItem("birthday" || null));
-
-  const lifeExpectancy = useCallback(() => {
-    if (!birthday) {
-      router.replace("/");
-      return
-    }
-    const lifeExpectancyBySex =
-      localStorage.getItem("sex") === SEX.FEMALE ? 80 : 83;
-
-    // 기대 수명
-    const lifeExpectancyDate =
-      `${String(parseInt(birthday?.substring(0, 4)) + lifeExpectancyBySex)}` +
-      String(birthday?.substr(4, 8));
-
-    const lifeExpectancySeconds = new Date(lifeExpectancyDate).getTime() / 1000;
-
-    // 현재
-    const now = Math.floor(new Date().getTime() / 1000);
-
-    // 남은 수명
-    const _livedSeconds = lifeExpectancySeconds - now;
-
-    setLifeSeconds(_livedSeconds);
-  }, []);
-
-  useEffect(() => {
-    // @TODO 백엔드 통신으로 모두 변경하자
-    lifeExpectancy();
-
-    const myInterval = setInterval(() => {
-      setLifeSeconds((lifeSeconds) => lifeSeconds - 1);
-    }, 1000);
-
-    return () => {
-      clearInterval(myInterval);
-    };
-  }, []);
-
-
-  const handleClickReset = useCallback(() => {
-    //@TODO 서버 로직으로 변경할거 예요~
-    removeUserInfo();
-    router.replace("/SettingPage");
-  }, [])
-
-  return (
-    <Container>
-      <Name>{name}님의 남은 시간</Name>
-      <LifeExpectancy>{lifeSeconds}</LifeExpectancy>
-      <Birthday>{birthday}</Birthday>
-      <SettingsBackupRestoreIcon onClick={handleClickReset} style={{ marginTop: '50px' }} sx={{ fontSize: 35 }} />
-    </Container>
-  );
-};
 
 const Container = styled.div`
   display: flex;
@@ -92,5 +33,65 @@ const Birthday = styled.div`
   font-size: ${FontSize.Small};
   opacity: 0.5;
 `
+
+const CountTimer = () => {
+  const router = useRouter();
+  const [lifeSeconds, setLifeSeconds] = useState(0);
+  const [name, setName] = useState(localStorage.getItem('name') || null)
+  const [birthday, setBirthDay] = useState(localStorage.getItem("birthday" || null));
+
+  const lifeExpectancy = useCallback(() => {
+    if (!birthday) {
+      router.replace("/");
+      return
+    }
+    const lifeExpectancyBySex =
+      localStorage.getItem("sex") === SEX.FEMALE ? 80 : 83;
+
+    // 기대 수명
+    const lifeExpectancyDate =
+      `${String(parseInt(birthday?.substring(0, 4), 10) + lifeExpectancyBySex)}${ 
+      String(birthday?.substr(4, 8))}`;
+
+    const lifeExpectancySeconds = new Date(lifeExpectancyDate).getTime() / 1000;
+
+    // 현재
+    const now = Math.floor(new Date().getTime() / 1000);
+
+    // 남은 수명
+    const _livedSeconds = lifeExpectancySeconds - now;
+
+    setLifeSeconds(_livedSeconds);
+  }, []);
+
+  useEffect(() => {
+    // @TODO 백엔드 통신으로 모두 변경하자
+    lifeExpectancy();
+
+    const myInterval = setInterval(() => {
+      setLifeSeconds((lifeSeconds) => lifeSeconds - 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(myInterval);
+    };
+  }, []);
+
+
+  const handleClickReset = useCallback(() => {
+    // @TODO 서버 로직으로 변경할거 예요~
+    removeUserInfo();
+    router.replace("/SettingPage");
+  }, [])
+
+  return (
+    <Container>
+      <Name>{name}님의 남은 시간</Name>
+      <LifeExpectancy>{lifeSeconds}</LifeExpectancy>
+      <Birthday>{birthday}</Birthday>
+      <SettingsBackupRestoreIcon onClick={handleClickReset} style={{ marginTop: '50px' }} sx={{ fontSize: 35 }} />
+    </Container>
+  );
+};
 
 export default CountTimer;
